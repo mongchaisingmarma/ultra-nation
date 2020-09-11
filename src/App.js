f5327ab4dd48c01c909cb33c5fda1a508b95d3a9
mongchaisingmarma <m.marma41413mongchaising@yahoo.com>
2020-09-12 01:09:21 +0600

    project done

--- src/App.js
+++ src/App.js
@@ -25,20 +25,6 @@ function App() {
     {
       countries.map(country => <Country country = {country} handleAddCountry = {handleAddCountry} key = {country.alpha3Code}></Country>)
     }
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
